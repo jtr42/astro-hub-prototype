0fadc8bff8e539f381045218439a332d7e0a9809
James Rappo <98123649+jtr42@users.noreply.github.com>
2026-09-04 16:34:11 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,28 +2,28 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://example.com",
-    integrations: [mdx(), sitemap(), react()],
-        vite: {
-            ssr: {
-          // Tells Astro/Vite to skip bundling these on the server completely
-          noExternal: [] 
-        },
-        build: {
-          rollupOptions: {
-            // Tells Rolldown to treat these imports as external modules
-            external: ['react-leaflet', 'leaflet'],
-          },
-    adapter: cloudflare({
-        platformProxy: {
-            enabled: true,
-        },
-    }),
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), react()],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
+  vite: {
+    ssr: {
+      // Tells Astro/Vite to skip bundling these on the server completely
+      noExternal: []
+    },
+    build: {
+      rollupOptions: {
+        // Tells Rolldown to treat these imports as external modules
+        external: ['react-leaflet', 'leaflet'],
+      },
+    },
+  },
 });
