00c95973c0d7da2ed7703a9f0cf743e3225d0d99
James Rappo <98123649+jtr42@users.noreply.github.com>
2026-09-04 15:29:29 -0400

    Update astro.config.mjs
    Returned to original format to avoid error when updating via astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,20 +4,14 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 import cloudflare from "@astrojs/cloudflare";
-import react from '@astrojs/react';
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  vite: {
-    ssr: {
-      // Tells Astro/Vite to skip bundling these on the server completely
-      noExternal: [] 
-    },
-    build: {
-      rollupOptions: {
-        // Tells Rolldown to treat these imports as external modules
-        external: ['react-leaflet', 'leaflet'],
-      },
-    },
-  },
+	site: "https://example.com",
+	integrations: [mdx(), sitemap()],
+	adapter: cloudflare({
+		platformProxy: {
+			enabled: true,
+		},
+	}),
 });
